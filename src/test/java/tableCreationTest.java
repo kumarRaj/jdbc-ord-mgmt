0803ab758f9373507a9b75f58b99f3fbeb915036
Raj <rajkum@thoughtworks.com>
2014-01-25 10:53:19 +0530

    Have a basic version of jdbc which runs tests

---
+++ src/test/java/tableCreationTest.java
@@ -0,0 +1,15 @@
+package test.java;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class tableCreationTest {
+    @Test
+    public void test_fail(){
+
+        assertEquals(1,1,1);
+    }
+}
